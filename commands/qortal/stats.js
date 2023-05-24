574bb264ec2b234b8712e363c257214d3d1c3453
QuickMythril <quickmythril@protonmail.com>
2023-05-24 05:01:49 -0400

    Changed stats command to use embed

--- commands/qortal/stats.js
+++ commands/qortal/stats.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
 const { request } = require('undici');
 
 module.exports = {
@@ -8,28 +8,41 @@ module.exports = {
 		.setDescription('Get Qortal stats & info.'),
 	category: 'qortal',
 	async execute(interaction) {
-		let statistics = "";
-    statistics += `__Qortal Stats & Info__\n\n`;
 
     const heightResponse = await request('https://api.qortal.org/blocks/height');
     const height = await heightResponse.body.json();
-    statistics += `Block Height: ${height}\n`;
-
     const reward = 5-(Math.floor((height-1)/259200))*0.25;
-    statistics += `Block Reward: ${reward} QORT\n`;
 
     const coreResponse = await request('https://api.github.com/repos/Qortal/qortal/releases/latest', {
       headers: { 'User-Agent': 'qombot' },
     });
     const coreObject = await coreResponse.body.json();
-    statistics += `Qortal Core: ${coreObject.tag_name}\n`;
 
     const uiResponse = await request('https://api.github.com/repos/Qortal/qortal-ui/releases/latest', {
       headers: { 'User-Agent': 'qombot' },
     });
     const uiObject = await uiResponse.body.json();
-    statistics += `Qortal UI: ${uiObject.tag_name}\n`;
 
-    return interaction.reply({ content: `${statistics}` });
+    const qortalStats = new EmbedBuilder()
+      .setColor(0x0099FF)
+      .setTitle('Qortal Stats & Info')
+      //.setURL('https://discord.js.org/')
+      //.setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+      //.setDescription('Some description here')
+      //.setThumbnail('https://i.imgur.com/AfFp7pu.png')
+      .addFields(
+        { name: 'Block Height', value: JSON.stringify(height), inline: true },
+        { name: 'Block Reward', value: `${reward} QORT`, inline: true },
+        { name: '\u200b', value: '\u200b', inline: true }, // empty field for spacing
+        { name: 'Core Version', value: JSON.stringify(coreObject.tag_name).replace(/"/g, ''), inline: true },
+        { name: 'UI Version', value: JSON.stringify(uiObject.tag_name).replace(/"/g, ''), inline: true },
+        { name: '\u200b', value: '\u200b', inline: true }, // empty field for spacing
+	      )
+	    //.addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
+	    //.setImage('https://i.imgur.com/AfFp7pu.png')
+	    .setTimestamp();
+	    //.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+
+    return interaction.reply({ embeds: [qortalStats] });
 	},
 };
