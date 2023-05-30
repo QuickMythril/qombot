234ccd0ecef80bf85fdcdb22448fb6510d4f9e62
QuickMythril <quickmythril@protonmail.com>
2023-05-29 23:39:51 -0400

    Added error handling to API requests

--- commands/qortal/stats.js
+++ commands/qortal/stats.js
@@ -8,20 +8,33 @@ module.exports = {
 		.setDescription('Get Qortal stats & info.'),
 	category: 'qortal',
 	async execute(interaction) {
+    let height, reward, coreObject, uiObject;
 
-    const heightResponse = await request('https://api.qortal.org/blocks/height');
-    const height = await heightResponse.body.json();
-    const reward = 5-(Math.floor((height-1)/259200))*0.25;
+    try {
+      const heightResponse = await request('https://api.qortal.org/blocks/height');
+      height = await heightResponse.body.json();
+      reward = 5-(Math.floor((height-1)/259200))*0.25;
+    } catch (error) {
+      console.error(`Error fetching block height: ${error.message}`);
+    }
 
-    const coreResponse = await request('https://api.github.com/repos/Qortal/qortal/releases/latest', {
-      headers: { 'User-Agent': 'qombot' },
-    });
-    const coreObject = await coreResponse.body.json();
+    try {
+      const coreResponse = await request('https://api.github.com/repos/Qortal/qortal/releases/latest', {
+        headers: { 'User-Agent': 'qombot' },
+      });
+      coreObject = await coreResponse.body.json();
+    } catch (error) {
+      console.error(`Error fetching Core version: ${error.message}`);
+    }
 
-    const uiResponse = await request('https://api.github.com/repos/Qortal/qortal-ui/releases/latest', {
-      headers: { 'User-Agent': 'qombot' },
-    });
-    const uiObject = await uiResponse.body.json();
+    try {
+      const uiResponse = await request('https://api.github.com/repos/Qortal/qortal-ui/releases/latest', {
+        headers: { 'User-Agent': 'qombot' },
+      });
+      uiObject = await uiResponse.body.json();
+    } catch (error) {
+      console.error(`Error fetching UI version: ${error.message}`);
+    }
 
     const qortalStats = new EmbedBuilder()
       .setColor(0x0099FF)
@@ -31,11 +44,11 @@ module.exports = {
       //.setDescription('Some description here')
       //.setThumbnail('https://i.imgur.com/AfFp7pu.png')
       .addFields(
-        { name: 'Block Height', value: JSON.stringify(height), inline: true },
-        { name: 'Block Reward', value: `${reward} QORT`, inline: true },
+        { name: 'Block Height', value: height !== undefined ? JSON.stringify(height) : 'Unavailable', inline: true },
+        { name: 'Block Reward', value: reward !== undefined ? `${reward} QORT` : 'Unavailable', inline: true },
         { name: '\u200b', value: '\u200b', inline: true }, // empty field for spacing
-        { name: 'Core Version', value: JSON.stringify(coreObject.tag_name).replace(/"/g, ''), inline: true },
-        { name: 'UI Version', value: JSON.stringify(uiObject.tag_name).replace(/"/g, ''), inline: true },
+        { name: 'Core Version', value: coreObject !== undefined ? JSON.stringify(coreObject.tag_name).replace(/\"/g, '') : 'Unavailable', inline: true },
+        { name: 'UI Version', value: uiObject !== undefined ? JSON.stringify(uiObject.tag_name).replace(/\"/g, '') : 'Unavailable', inline: true },
         { name: '\u200b', value: '\u200b', inline: true }, // empty field for spacing
 	      )
 	    //.addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
